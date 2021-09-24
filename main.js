319d541cf6fc3369a5f82a6834b808595b182e9e
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2021-09-24 21:07:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,35 @@ function draw(){
     image(video,0,0,700,600);
 
 }
+
+function draw(){
+
+    background(0); 
+    image(video, 0, 0, 700, 600);
+  
+    fill("black");
+    stroke("black");
+    rect(680,0,20,700);
+  
+    fill("black");
+    stroke("black");
+    rect(0,0,20,700);}
+    
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
